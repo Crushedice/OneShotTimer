9eca83c0d40ccb6218a381c071746e0a6ec7e343
Crushed Ice <tenax92@gmx.de>
2021-11-06 23:27:18 +0100

    Add project files.

---
+++ TimerTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using OneShotTimer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneShotTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start");
+
+
+            Console.WriteLine("Do new timer");
+
+            _ = new OST(Thisone,3);
+
+            Console.ReadKey();
+        }
+
+        public static void Thisone()
+        {
+
+            Console.WriteLine("Method Called ! ");
+
+        }
+    }
+}
